8d164d1908d1da8984bbd9f3c9bf38db51b64cc0
Hrutik Mehta <hrutikmehta@gmail.com>
2021-03-29 19:19:18 -0400

    COGS format change

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -46,7 +46,7 @@ class Bot(BotBase):
 
     def setup(self):
         for cog in COGS:
-            self.load_extension(os.path.abspath(os.path.join(os.path.split(currDir)[0], "cogs", cog, ".py")))
+            self.load_extension(os.path.abspath(os.path.join(os.path.split(currDir)[0], "cogs", cog)))
         print("setup complete")
 
     def run(self, version):
